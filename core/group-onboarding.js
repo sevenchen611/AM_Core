2e4dc39a3c0b10dd8dd613eb94dbc1cb5e740def
sevenchen611 <266079235+sevenchen611@users.noreply.github.com>
2026-08-05 18:21:57 +0800

    Mark scoped dashboard deployment

--- core/group-onboarding.js
+++ core/group-onboarding.js
@@ -6,7 +6,7 @@
 
 import { textItem } from './util.js';
 
-export const GROUP_ONBOARDING_BUILD = 'group-name-repair-2026-08-03';
+export const GROUP_ONBOARDING_BUILD = 'scoped-dashboard-2026-08-05';
 export const LEGACY_HOZO20_BIND_COMMAND = '<绑定 HOZOAM 2.0 群组>';
 
 export const ONBOARDING_TENANTS = [
